48734d3799f4423af248ecafca5ad023deab6f7d
Martin Ambrus <martin@widemedia.cz>
2012-02-23 21:47:21 +0100

    commands autoloading updated

--- CommandsEX/src/com/github/zathrus_writer/commandsex/CommandsEX.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/CommandsEX.java
@@ -1,6 +1,5 @@
 package com.github.zathrus_writer.commandsex;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,15 +17,15 @@ public class CommandsEX extends JavaPlugin {
 	public final String intRegex = "(-)?(\\d){1,10}(\\.(\\d){1,10})?";
 	
 	// logger
-	public final Logger l = Logger.getLogger("Minecraft");
+	public final static Logger LOGGER = Logger.getLogger("Minecraft");
 	
 	// language constants
-	public final String langInWorldCommandOnly = "This command can only be used in-world.";
-	public final String langInsufficientPerms = "You don't have sufficient permissions to use this command.";
-	public final String langEnableMsg = " has successfully started.";
-	public final String langDisableMsg = " has been disabled.";
-	public final String langVersion = "version";
-	public final String langInternalError = "An internal error has occured. Please try again or contact an administrator.";
+	public final static String langInWorldCommandOnly = "This command can only be used in-world.";
+	public final static String langInsufficientPerms = "You don't have sufficient permissions to use this command.";
+	public final static String langEnableMsg = " has successfully started.";
+	public final static String langDisableMsg = " has been disabled.";
+	public final static String langVersion = "version";
+	public final static String langInternalError = "An internal error has occured. Please try again or contact an administrator.";
 	
 	// classnames of all our plugin components
 	public final String[] extenders = {"Teleportation"};
@@ -36,11 +35,7 @@ public class CommandsEX extends JavaPlugin {
 	//       through these values for the "command:" string and then we can dynamically invoke our function along with our parameters
 	public List<String> commands = new ArrayList<String>();
 	
-	// holds instances of successfully loaded classes, so we don't re-initialize them again when we need them later
-	private List<Class<?>> extenderClassInstances = new ArrayList<Class<?>>();
-	
-	
-	
+
 	/***
 	 * Checks if the CommandSender is a Player, gives the sender error message if he's not and returns Boolean value.
 	 * @param cs
@@ -49,10 +44,10 @@ public class CommandsEX extends JavaPlugin {
 	public Boolean checkIsPlayer(CommandSender cs) {
 		if (cs instanceof Player) {
 			return true;
-		} else {
-			cs.sendMessage(ChatColor.RED + langInWorldCommandOnly);
-			return false;
 		}
+
+		cs.sendMessage(ChatColor.RED + langInWorldCommandOnly);
+		return false;
 	}
 	
 	
@@ -80,26 +75,36 @@ public class CommandsEX extends JavaPlugin {
 	 * OnEnable
 	 */
 	public void onEnable() {
-		// load all extending classes we have listed in extenders array and let them initialize their stuff (commands, events...)
-		Class<?>[] proto = new Class[] {CommandsEX.class};
+		// load all extending classes we have listed in extenders array, call their init() functions
+		// and add all functions they offer for users into our commands list
+		Class<?>[] proto = new Class[] {this.getClass()};
 		Object[] params = new Object[] {this};
 
 		for (String cName : this.extenders) {
 			try {
+				// no need to create instances here, just call init() and let them all live in peace
 				Class<?> c = Class.forName("com.github.zathrus_writer.commandsex." + cName);
-				Constructor<?> ct = c.getConstructor(proto);
-				Object obj = ct.newInstance(params);
-				extenderClassInstances.add(obj.getClass());
+				Method method = c.getDeclaredMethod("init", proto);
+				method.invoke(null, params);
+
+				// we only add public methods as command functions
+				Method[] methods = c.getMethods();
+				for (Method m : methods) {
+					String n = m.getName();
+					if (n.startsWith("_")) {
+						this.commands.add(n + ":" + c.getSimpleName());
+					}
+				}
 			} catch(ClassNotFoundException e) {
 				// all ok if class was not found, this is a modular plugin
 		    } catch(Exception e) {
 		    	// all other errors, we need to log these to console
-		    	this.l.severe("Error loading class " + cName + " (" + e.getMessage() + ")");
+		    	LOGGER.severe("Error loading class " + cName + " (" + e.getMessage() + ")");
 		    }
 		}
 
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.l.info(pdfFile.getName() + " " + langVersion + " " + pdfFile.getVersion() + langEnableMsg);
+		LOGGER.info(pdfFile.getName() + " " + langVersion + " " + pdfFile.getVersion() + langEnableMsg);
 	}
 
 	
@@ -107,7 +112,7 @@ public class CommandsEX extends JavaPlugin {
 	 * OnDisable
 	 */
 	public void onDisable() {
-		this.l.info(this.getDescription().getName() + langDisableMsg);
+		LOGGER.info(this.getDescription().getName() + langDisableMsg);
 	}
 	
 	
@@ -123,28 +128,25 @@ public class CommandsEX extends JavaPlugin {
     		// check if the command name if present in our list of commands, and if so, pass action to the appropriate function
     		for (String sCommand : this.commands) {
 				// check for the correct class to be used
-				if (sCommand.startsWith(cmd + ":")) {
+				if (sCommand.startsWith("_" + cmd + ":")) {
 					String[] s = sCommand.split(":");
-					for (Class<?> c : extenderClassInstances) {
-						if (c.getSimpleName().equals(s[1])) {
-							try {
-								Class<?>[] proto = new Class[] {Player.class, String[].class};
-								Object[] params = new Object[] {player, args};
-								Method method = c.getDeclaredMethod(s[0], proto);
-								Object ret = method.invoke(c, params);
-								return Boolean.TRUE.equals(ret);
-							} catch (Throwable e) {
-								player.sendMessage(ChatColor.RED + this.langInternalError);
-			    				this.l.severe("Couldn't handle function call " + cmd + " (" + s[0] + ") for class " + s[1] + ", error returned: " + e.getMessage());
-			    				return true;
-			    			}
-						}
-		    		}
-					
-					// we should never get here, except for errors
-					player.sendMessage(ChatColor.RED + this.langInternalError);
-	        		this.l.severe("Command /" + cmd + " did not match any known function for player "+ player.getDisplayName() +"!");
+					try {
+						Class<?>[] proto = new Class[] {Player.class, String[].class};
+						Object[] params = new Object[] {player, args};
+						Class<?> c = Class.forName("com.github.zathrus_writer.commandsex." + s[1]);
+						Method method = c.getDeclaredMethod(s[0], proto);
+						Object ret = method.invoke(null, params);
+						return Boolean.TRUE.equals(ret);
+					} catch (Throwable e) {
+						player.sendMessage(ChatColor.RED + langInternalError);
+						LOGGER.severe("Couldn't handle function call " + cmd + " (" + s[0] + ") for class " + s[1] + ", error returned: " + e.getMessage());
+			    		return true;
+			    	}
 				}
+					
+				// we should never get here, except for errors
+				player.sendMessage(ChatColor.RED + langInternalError);
+				LOGGER.severe("Command /" + cmd + " did not match any known function for player "+ player.getDisplayName() +"!");
     		}
     	}
         return true;
