934834e14e8aee01b1843e1b61ecc6f4969a896c
Martin Ambrus <martin@widemedia.cz>
2012-04-17 12:59:04 +0200

    added /online command, thus moving on to the Beta phase!

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_online.java
@@ -0,0 +1,57 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.zathrus_writer.commandsex.CommandsEX;
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+import com.github.zathrus_writer.commandsex.helpers.Permissions;
+import com.github.zathrus_writer.commandsex.helpers.Utils;
+
+public class Command_cex_online {
+	
+	public static Map<String, Location> lastLocations = new HashMap<String, Location>();
+	
+	/***
+	 * ONLINE - displays list of all players that are currently online
+	 * @param sender
+	 * @param args
+	 * @return
+	 */
+	public static Boolean run(CommandSender sender, String alias, String[] args) {
+		Boolean hasPerms = true;
+		if (sender instanceof Player) {
+			hasPerms = Permissions.checkPerms((Player)sender, "cex.online");
+		}
+		
+		// permissions ok, list players
+		if (hasPerms) {
+			if (args.length == 0) {
+				// list all players
+				int playersOnline = CommandsEX.plugin.getServer().getOnlinePlayers().length;
+				String[] players = new String[playersOnline];
+				for(int i = 0; i < playersOnline; i++){
+				    players[i] = CommandsEX.plugin.getServer().getOnlinePlayers()[i].getName();
+				}
+
+				LogHelper.showInfo("onlinePlayers#####[" + Utils.implode(players, ","), sender);
+			} else {
+				// check if a given player is online
+				Player p = Bukkit.getServer().getPlayer(args[0]);
+				if (p == null) {
+					LogHelper.showInfo("[" + args[0] + " #####isOffline", sender);
+				} else {
+					LogHelper.showInfo("[" + p.getName() + " #####isOnline", sender);
+				}
+			}
+		}
+
+        return true;
+	}
+}
