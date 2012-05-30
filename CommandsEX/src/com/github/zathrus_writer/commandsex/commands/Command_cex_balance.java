d3975afd1a217009833f7f005df071fba6411651
kezz101 <1millionchances@googlemail.com>
2012-05-30 20:55:08 +0100

    Added Balance command

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_balance.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_balance.java
@@ -2,9 +2,23 @@ package com.github.zathrus_writer.commandsex.commands;
 
 import org.bukkit.command.CommandSender;
 
+import com.github.zathrus_writer.commandsex.Vault;
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+import static com.github.zathrus_writer.commandsex.Language._;
+
 public class Command_cex_balance {
 	public static Boolean run(CommandSender sender, String alias, String[] args) {
-		Double balance = Economy.getBalance(Player sender);
+		
+		if(Vault.ecoEnabled() != true) {
+			 LogHelper.logSevere(_("economyNotFound", ""));
+			 LogHelper.showInfo("economyNotFound#####", sender);
+		}
+		
+		String player = sender.getName();
+		
+		double balance = Vault.econ.getBalance(player);
+		
+		LogHelper.showInfo("economyBalance#####" + balance, sender);
 		
 		return true;
 	}
