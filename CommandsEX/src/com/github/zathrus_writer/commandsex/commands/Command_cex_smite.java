d87b9d50fd0dcbcd98c08c605f3822bc126fedda
kezz101 <1millionchances@googlemail.com>
2012-05-31 21:07:43 +0100

    Added /cex_smite command

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_smite.java
@@ -0,0 +1,48 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.zathrus_writer.commandsex.CommandsEX;
+import com.github.zathrus_writer.commandsex.helpers.Commands;
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+
+public class Command_cex_smite {
+	
+	/***
+	 * SMITE - kills a player with LIGHTNING
+	 * @param sender
+	 * @param args
+	 * @return
+	 */
+	
+	public static Boolean run(CommandSender sender, String alias, String[] args) {
+		if(args.length > 0) {
+			// get variables about the player 
+			Player smited = Bukkit.getServer().getPlayer(args[0]);
+			Location loc = smited.getLocation();
+		
+			// smite the player
+			smited.getWorld().strikeLightningEffect(loc);
+			smited.setHealth(0);
+		
+			// show the sender a message
+			LogHelper.showInfo("smitePlayer#####[" + smited, sender, ChatColor.AQUA);
+			
+			// config variable
+			Boolean showMessageOnSmite = CommandsEX.getConf().getBoolean("showMessageOnSmite");
+			
+			// show who smited the smitee (is that a word)
+			if(showMessageOnSmite == true) {
+				LogHelper.showInfo("smiteRecieveSmite#####[" + sender, smited, ChatColor.RED);
+			}
+			
+		} else {
+			Commands.showCommandHelpAndUsage(sender, "cex_smite", alias);
+		}
+		return true;
+	}
+}
