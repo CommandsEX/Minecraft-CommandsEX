26371a719afda295785d38b164be3ebe971b75ac
Martin Ambrus <martin@widemedia.cz>
2012-06-18 16:27:52 +0200

    adjusted check for sender != console

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_heal.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_heal.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 
 import com.github.zathrus_writer.commandsex.helpers.LogHelper;
 import com.github.zathrus_writer.commandsex.helpers.Permissions;
+import com.github.zathrus_writer.commandsex.helpers.PlayerHelper;
 
 public class Command_cex_heal {
 
@@ -19,31 +20,33 @@ public class Command_cex_heal {
 	
 	public static Boolean run(CommandSender sender, String alias, String[] args){
 		
-		if (!(sender instanceof Player) && args.length == 0){
-			LogHelper.showWarning("playerNameMissing", sender);
-			return true;
-		}
-		
-		Player beingHealed;
-		
-		if ((args.length > 0) && (!(sender instanceof Player) || (Permissions.checkPerms((Player) sender, "cex.heal.others")))){
-			beingHealed = Bukkit.getPlayer(args[0]);
-			
-			if (beingHealed == null){
-				LogHelper.showWarning("invalidPlayer", sender);
+		if (PlayerHelper.checkIsPlayer(sender)) {
+			if (args.length == 0){
+				LogHelper.showWarning("playerNameMissing", sender);
 				return true;
 			}
-		} else {
-			beingHealed = (Player) sender;
-		}
-		
-		beingHealed.setHealth(20);
-		
-		if (sender.getName().equalsIgnoreCase(beingHealed.getName())){
-			LogHelper.showInfo("healHealed", beingHealed);
-		} else {
-			LogHelper.showInfo("healHealedBySomeoneElse", beingHealed);
-			LogHelper.showInfo("healHealedSomeoneElse######" + "[" + beingHealed.getName(), sender);
+			
+			Player beingHealed;
+			
+			if ((!(sender instanceof Player) || (Permissions.checkPerms((Player) sender, "cex.heal.others")))) {
+				beingHealed = Bukkit.getPlayer(args[0]);
+				
+				if (beingHealed == null){
+					LogHelper.showWarning("invalidPlayer", sender);
+					return true;
+				}
+			} else {
+				beingHealed = (Player) sender;
+			}
+			
+			beingHealed.setHealth(20);
+			
+			if (sender.getName().equalsIgnoreCase(beingHealed.getName())){
+				LogHelper.showInfo("healHealed", beingHealed);
+			} else {
+				LogHelper.showInfo("healHealedBySomeoneElse", beingHealed);
+				LogHelper.showInfo("healHealedSomeoneElse######" + "[" + beingHealed.getName(), sender);
+			}
 		}
 		
 		return true;
