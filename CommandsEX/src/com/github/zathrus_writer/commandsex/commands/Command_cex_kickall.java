286ea6e472cd3d722e2bd5920963f0b39270daec
iKeirNez <keir@nellyer.co.uk>
2012-06-18 20:11:41 +0100

    Added a kickall command with a customizable message.

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_kickall.java
@@ -0,0 +1,49 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+
+import static com.github.zathrus_writer.commandsex.Language._;
+
+public class Command_cex_kickall {
+
+	/***
+	 * KICKALL - Kicks all players without permission from the server, with customizable message
+	 * @param sender
+	 * @param args
+	 * @return
+	 */
+	
+	public static Boolean run(CommandSender sender, String alias, String[] args){
+		
+		if (!(sender instanceof Player) || sender.hasPermission("cex.kickall")){
+			if (args.length == 0){
+				for (Player player : Bukkit.getOnlinePlayers()){
+					if (!player.hasPermission("cex.kick.excempt") && player != sender){
+						player.kickPlayer(_("kickGenericReason", player.getName()));
+					}
+				}
+				
+				LogHelper.showInfo("kickAllSuccess", sender, ChatColor.GREEN);
+			} else {
+				String kickMessage = StringUtils.join(args, ' ');
+				
+				for (Player player : Bukkit.getOnlinePlayers()){
+					if (!player.hasPermission("cex.kick.excempt") && player != sender){
+						player.kickPlayer(kickMessage);
+					}
+				}
+				
+				LogHelper.showInfo("kickAllSuccessReason#####[" + " " + kickMessage, sender, ChatColor.GREEN);
+			}
+		}
+		
+		return true;
+	}
+	
+}
