40c9a7a2a3555cfd88ab97b7ebfbf02c38f0db08
iKeirNez <keir@nellyer.co.uk>
2012-06-22 19:57:45 +0100

    Added a synonym for /warp delete, /warp remove

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_warp.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_warp.java
@@ -51,11 +51,11 @@ public class Command_cex_warp extends Warps {
 					 * WARP HELP
 					 */
 					help(sender, args, "warp", alias);
-				} else if ((args.length > 1) && args[0].toLowerCase().equals("delete")) {
+				} else if ((args.length > 1) && (args[0].toLowerCase().equals("delete") || (args[0].toLowerCase().equals("remove")))) {
 					/***
 					 * WARP DELETE
 					 */
-					if ((args.length > 1) && args[0].toLowerCase().equals("delete") && (hasPerms = Permissions.checkPerms(player, "cex.warp.delete"))) {
+					if ((hasPerms = Permissions.checkPerms(player, "cex.warp.delete"))) {
 						delete(sender, args, "warp", alias);
 					}
 				} else if ((args.length == 1) && (Permissions.checkPerms(player, "cex.warp.own") || Permissions.checkPerms(player, "cex.warp")) && hasPerms) {
