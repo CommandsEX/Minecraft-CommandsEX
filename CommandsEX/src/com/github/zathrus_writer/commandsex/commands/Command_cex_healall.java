fd5f698b3590afc4f29823dd877e263af9dca6c2
Martin Ambrus <martin@widemedia.cz>
2012-06-24 00:26:53 +0200

    no need for the additional "CONSOLE" text, Bukkit always returns CONSOLE for a console command sender

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_healall.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_healall.java
@@ -19,20 +19,11 @@ public class Command_cex_healall {
 	 */
 	
 	public static Boolean run(CommandSender sender, String alias, String[] args){
-		
-		String healedBy;
-		
-		if (sender instanceof Player){
-			Player p = (Player) sender;
-			healedBy = p.getName();
-		} else {
-			healedBy = "*CONSOLE*";
-		}
-		
+
 		if (args.length == 0){
 			for (Player p : Bukkit.getOnlinePlayers()){
 				p.setHealth(20);
-				LogHelper.showInfo("healHealedBySomeoneElse#####[" + healedBy, p, ChatColor.AQUA);
+				LogHelper.showInfo("healHealedBySomeoneElse#####[" + sender.getName(), p, ChatColor.AQUA);
 			}
 			LogHelper.showInfo("healAllSuccess", sender, ChatColor.GREEN);
 		} else {
