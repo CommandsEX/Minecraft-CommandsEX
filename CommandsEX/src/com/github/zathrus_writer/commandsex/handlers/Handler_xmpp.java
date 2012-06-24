70b08c19e77ff59ea8c116d6c6077aa242a357ac
iKeirNez <keir@nellyer.co.uk>
2012-06-24 14:54:28 +0100

    XMPP Fixes

--- CommandsEX/src/com/github/zathrus_writer/commandsex/handlers/Handler_xmpp.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/handlers/Handler_xmpp.java
@@ -1,12 +1,12 @@
 package com.github.zathrus_writer.commandsex.handlers;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.github.zathrus_writer.commandsex.CommandsEX;
 import com.github.zathrus_writer.commandsex.helpers.ClasspathHacker;
 import com.github.zathrus_writer.commandsex.helpers.LogHelper;
 import com.github.zathrus_writer.commandsex.helpers.XMPPer;
-import static com.github.zathrus_writer.commandsex.Language._;
 
 public class Handler_xmpp {
 
@@ -15,18 +15,22 @@ public class Handler_xmpp {
 	 * @param plugin
 	 */
 	public Handler_xmpp() {
-		// first of all, load smack libraries
-		try {
-			ClasspathHacker.addFile(CommandsEX.plugin.getDataFolder() + "/smack.jar");
-			ClasspathHacker.addFile(CommandsEX.plugin.getDataFolder() + "/smackx.jar");
-		} catch (IOException e) {
-			LogHelper.logSevere("[CommandsEX] " + _("xmppDownloadSmack", ""));
+		// Check if the files exist, if they do the load the libaries and startup XMPPer
+		if (new File(CommandsEX.plugin.getDataFolder() + "/smack.jar").exists() && new File(CommandsEX.plugin.getDataFolder() + "/smackx.jar").exists()){
+			try {
+				ClasspathHacker.addFile(CommandsEX.plugin.getDataFolder() + "/smack.jar");
+				ClasspathHacker.addFile(CommandsEX.plugin.getDataFolder() + "/smackx.jar");
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+
+			// now initialize the actual XMPP communication handling class if smack is installed
+			new XMPPer();
+		} else {
+			LogHelper.logSevere("xmppDownloadSmack");
 		}
-		
-		// now initialize the actual XMPP communication handling class
-		new XMPPer();
 	}
-	
+
 	/***
 	 * Closes XMPP connection on plugin disable.
 	 */
