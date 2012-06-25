5a0fd9ffe0c0df647a93946d6fb1fffac7ea2de5
iKeirNez <keir@nellyer.co.uk>
2012-06-25 18:50:03 +0100

    Few changes to /slapall

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_slapall.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_slapall.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.github.zathrus_writer.commandsex.helpers.Commands;
 import com.github.zathrus_writer.commandsex.helpers.Common;
 import com.github.zathrus_writer.commandsex.helpers.LogHelper;
 import com.github.zathrus_writer.commandsex.helpers.Utils;
@@ -12,36 +13,34 @@ import com.github.zathrus_writer.commandsex.helpers.Utils;
 public class Command_cex_slapall extends Common {
 
 	public static Boolean run(CommandSender sender, String alias, String[] args){
-		
+
 		if (sender instanceof Player){
 			Player player = (Player) sender;
 			if (Utils.checkCommandSpam(player, "cex_slapall")){
 				return true;
 			}
 		}
-		
-		if (!(sender instanceof Player) || (!Utils.checkCommandSpam((Player) sender, "slap"))) {
-			if (args.length > 1){
-				// Incorrect args
-				return true;
-			}
-			
-			String slapHeight = null;
-			if (args.length == 1){
-				slapHeight = args[0];
-			}
-			
-			for (Player p : Bukkit.getOnlinePlayers()){
-				// Don't launch the sender
-				if (!sender.getName().equalsIgnoreCase(p.getName())){
-					String[] newargs = {p.getName(), (slapHeight != null ? slapHeight : "")};
-					slap(sender, newargs, "slapall", alias);
-				}
-			}
-			
-			LogHelper.showInfo("playerSlappedAll", sender, ChatColor.AQUA);
+
+		if (args.length > 1){
+			Commands.showCommandHelpAndUsage(sender, "cex_slapall", alias);
+			return true;
 		}
+
+		String slapHeight = null;
+		if (args.length == 1){
+			slapHeight = args[0];
+		}
+
+		for (Player p : Bukkit.getOnlinePlayers()){
+			// Don't launch the sender
+			if (!sender.getName().equalsIgnoreCase(p.getName())){
+				String[] newargs = {p.getName(), (slapHeight != null ? slapHeight : "")};
+				slap(sender, newargs, "slapall", alias);
+			}
+		}
+
+		LogHelper.showInfo("playerSlappedAll", sender, ChatColor.AQUA);
 		return true;
 	}
-	
+
 }
