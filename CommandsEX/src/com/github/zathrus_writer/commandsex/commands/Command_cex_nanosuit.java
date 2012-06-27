13a528b45c469b86c6b67ba3d7e9781ac0302b30
iKeirNez <keir@nellyer.co.uk>
2012-06-27 21:30:49 +0100

    Whoops, forgot to commit this new command for nanosuit

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_nanosuit.java
@@ -0,0 +1,18 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+import org.bukkit.command.CommandSender;
+
+import com.github.zathrus_writer.commandsex.helpers.PlayerHelper;
+
+public class Command_cex_nanosuit {
+
+	public static Boolean run(CommandSender sender, String alias, String[] args){
+		
+		if (PlayerHelper.checkIsPlayer(sender)){
+			
+		}
+		
+		return true;
+	}
+	
+}
