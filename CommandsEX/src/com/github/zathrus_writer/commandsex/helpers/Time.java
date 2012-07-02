a8a5b2b8f4008e8466987f9b30b775da00016a2c
iKeirNez <keir@nellyer.co.uk>
2012-07-02 17:25:29 +0100

    Added /ptime command.

--- CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/Time.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/Time.java
@@ -1,6 +1,7 @@
 package com.github.zathrus_writer.commandsex.helpers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -77,5 +78,75 @@ public class Time {
 				LogHelper.showInfo("timeChangedBy1#####[" + worldName + " #####timeChangedBy2#####[" + pName, player);
 			}
 		}
+	}
+	
+	/***
+	 * Function to set a players time
+	 * @author iKeirNez
+	 * @param sender
+	 * @param args
+	 */
+	
+	public static void setPlayerTime(CommandSender sender, String[] args) {
+		Player p = (Player) sender;
+		// Reset the player time to server time
+		if (args[0].equalsIgnoreCase("reset")){
+			p.resetPlayerTime();
+			LogHelper.showInfo("playerTimeReset", sender, ChatColor.AQUA);
+			return;
+		}
+		
+		// we only support SET and ADD
+		if (!args[0].equalsIgnoreCase("set") && !args[0].equalsIgnoreCase("add")) {
+			LogHelper.showWarning("timeInvalidFirstArgument", sender);
+			return;
+		}
+		
+		// for ADD, the value must be numeric
+		if (args[0].equalsIgnoreCase("add") && ((args.length < 2) || !args[1].matches(CommandsEX.intRegex))) {
+			LogHelper.showWarning("timeAddOnlyWorksWithNumbers", sender);
+			return;
+		}
+		
+		// convert non-numeric second parameter to a numeric one
+		Long moveBy = 0L;
+		if (args[1].matches(CommandsEX.intRegex)) {
+			moveBy = Long.parseLong(args[1]);
+
+			// check if we are within allowed constrains
+			if (moveBy < 0) {
+				LogHelper.showWarning("timeCannotBeNegative", sender);
+				return;
+			}
+			
+			if (args[0].equalsIgnoreCase("set") && (moveBy > 24000)) {
+				LogHelper.showWarning("timeOutOfBounds", sender);
+				return;
+			}
+		} else {
+			if (args[1].equalsIgnoreCase("dawn")) {
+				moveBy = 0L;
+			} else if (args[1].equalsIgnoreCase("morning")) {
+				moveBy = 2500L;
+			} else if (args[1].equalsIgnoreCase("noon")) {
+				moveBy = 6000L;
+			} else if (args[1].equalsIgnoreCase("evening")) {
+				moveBy = 11000L;
+			} else if (args[1].equalsIgnoreCase("night")) {
+				moveBy = 15000L;
+			} else {
+				LogHelper.showWarning("timeInvalidStringValue", sender);
+				return;
+			}
+		}
+		
+		if (args[0].equalsIgnoreCase("set")) {
+			p.setPlayerTime(moveBy, false);
+		} else {
+			p.setPlayerTime(p.getPlayerTime() + moveBy, false);
+		}
+		
+		// tell the player and anyone with permission to see what happened
+		LogHelper.showInfo("playerTimeChanged#####[" + p.getPlayerTime(), sender);
 	}
 }
