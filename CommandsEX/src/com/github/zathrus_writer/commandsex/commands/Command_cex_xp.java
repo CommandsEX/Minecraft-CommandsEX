dd9748bd4b6e686b96b495f5ab3fd95d4c5a0c29
iKeirNez <keir@nellyer.co.uk>
2012-07-03 16:38:28 +0100

    Start of /xp command, still some work to do

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_xp.java
@@ -0,0 +1,119 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.zathrus_writer.commandsex.CommandsEX;
+import com.github.zathrus_writer.commandsex.helpers.Commands;
+import com.github.zathrus_writer.commandsex.helpers.ExperienceManager;
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+
+public class Command_cex_xp {
+
+	public static Boolean run(CommandSender sender, String alias, String[] args){
+		
+		boolean validCommand;
+		
+		if (args.length == 1){
+			if (args[0].equalsIgnoreCase("view")){
+				validCommand = true;
+			} else {
+				validCommand = false;
+			}
+		} else if (args.length == 2 || args.length == 3){
+			validCommand = true;
+		} else {
+			validCommand = false;
+		}
+		
+		if (!validCommand){
+			Commands.showCommandHelpAndUsage(sender, "cex_xp", alias);
+			return true;
+		}
+
+		Player target;
+		String function;
+		String amount;
+		int amountint = 0;
+
+		if ((args.length == 3 && !args[1].equalsIgnoreCase("view")) || (args.length == 2 && args[1].equalsIgnoreCase("view"))){
+			target = Bukkit.getPlayer(args[0]);
+			function = args[1];
+		} else {
+			if (sender instanceof Player){
+				target = ((Player) sender);
+			} else {
+				Commands.showCommandHelpAndUsage(sender, "cex_xp", alias);
+				return true;
+			}
+			function = args[0];
+		}
+
+		if (target == null){
+			LogHelper.showInfo("invalidPlayer", sender, ChatColor.RED);
+			return true;
+		}
+
+		if (!function.equalsIgnoreCase("view") && !function.equalsIgnoreCase("set") && !function.equalsIgnoreCase("add") && !function.equalsIgnoreCase("take")){
+			Commands.showCommandHelpAndUsage(sender, "cex_xp", alias);
+			return true;
+		}
+		
+		if (!function.equalsIgnoreCase("view")){
+			if (args.length == 2){
+				amount = args[1];
+			} else {
+				amount = args[2];
+			}
+			
+			if (amount.matches(CommandsEX.intRegex)){
+				amountint = Integer.valueOf(amount);
+			} else {
+				LogHelper.showInfo("xpNotNumeric", sender, ChatColor.RED);
+				return true;
+			}
+		}
+		
+		ExperienceManager expman = new ExperienceManager(target);
+		
+		if (function.equalsIgnoreCase("view")){
+			LogHelper.showInfo((sender != target ? "#####[" + target.getName() + " #####xpHas#####[" : "xpViewSelf#####[") + expman.getCurrentExp() + " #####xpExperience", sender, ChatColor.AQUA);
+			return true;
+		}
+		
+		int xpLimit = 438247;
+		
+		if (function.equalsIgnoreCase("set")){
+			boolean canSet = (amountint > xpLimit ? false : true);
+			expman.setExp((canSet ? amountint : xpLimit));
+			if (!canSet){ LogHelper.showInfo("xpCouldNotAddAll", sender, ChatColor.RED); }
+			if (sender != target) { LogHelper.showInfo("xpSet#####[" + target.getName() +  " #####xpTo#####[" + (canSet ? amountint : xpLimit), sender, ChatColor.AQUA); }
+			LogHelper.showInfo((sender != target ? "#####[" + sender.getName() + " #####xpSetMsgToTarget1#####[" : "xpSetMsgToTarget2#####[") + (canSet ? amountint : xpLimit), target, ChatColor.AQUA);
+		}
+		
+		if (function.equalsIgnoreCase("add")){
+			boolean spaceToAdd = (expman.getCurrentExp() + amountint > xpLimit ? false : true);
+			expman.changeExp(amountint);
+			if (amountint - xpLimit > 0){
+				if (!spaceToAdd) { LogHelper.showInfo("xpCouldNotAddAll", sender, ChatColor.RED); }
+				if (sender != target) { LogHelper.showInfo("xpAdded#####[" + (spaceToAdd ? amountint : (amountint - xpLimit < 0 ? 0 : amountint - xpLimit)) + " #####xpExperience#####[ #####xpTo#####[" + target.getName(), sender, ChatColor.GREEN); }
+				LogHelper.showInfo((sender != target ? "#####[" + sender.getName() + " #####xpAddedGave1#####[" : "xpAddedGave2#####[") + (spaceToAdd ? amountint : amountint - xpLimit) + " #####xpExperience", target, ChatColor.AQUA);
+			} else {
+				
+			}
+		}
+		
+		if (function.equalsIgnoreCase("take")){
+			boolean hasXP = expman.hasExp(amountint);
+			int oldXP = expman.getCurrentExp();
+			expman.changeExp(-(hasXP ? amountint : expman.getCurrentExp()));
+			if (!hasXP) { LogHelper.showInfo("xpNotAllRemoved", sender, ChatColor.RED); }
+			if (sender != target) { LogHelper.showInfo("xpTaken#####[" + (hasXP ? amountint : oldXP) + " #####xpExperience#####[ #####xpFrom#####[" + target.getName(), sender, ChatColor.GREEN); }
+			LogHelper.showInfo((sender != target ? "#####[" + sender.getName() + " #####xpTakenTook1#####[" : "xpTakenTook2#####[") + (hasXP ? amountint : oldXP) + " #####xpExperience", target, ChatColor.AQUA);
+		}
+		return true;
+	}
+	
+}
---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/ExperienceManager.java
@@ -0,0 +1,156 @@
+package com.github.zathrus_writer.commandsex.helpers;
+
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+/**
+ * @author des
+ *
+ * Adapted from ExperienceUtils code originally in ScrollingMenuSign.
+ * 
+ * Credit to nisovin (http://forums.bukkit.org/threads/experienceutils-make-giving-taking-exp-a-bit-more-intuitive.54450/#post-1067480)
+ * for an implementation that avoids the problems of getTotalExperience(), which doesn't work properly after a player has enchanted something.
+ */
+public class ExperienceManager {
+	private static int xpRequiredForNextLevel[];
+	private static int xpTotalToReachLevel[];
+
+	private final String playerName;
+
+	static {
+		initLookupTables(500);
+	}
+
+	/**
+	 * Initialise the XP lookup tables.  Basing this on observations noted in https://bukkit.atlassian.net/browse/BUKKIT-47
+	 * 
+	 * 7 xp to get to level 1, 17 to level 2, 31 to level 3...
+	 * At each level, the increment to get to the next level increases alternately by 3 and 4
+	 * 
+	 * @param maxLevel	The highest level handled by the lookup tables
+	 */
+	private static void initLookupTables(int maxLevel) {
+		xpRequiredForNextLevel = new int[maxLevel];
+		xpTotalToReachLevel = new int[maxLevel];
+
+		xpTotalToReachLevel[0] = 0;
+		int incr = 7;
+		for (int i = 1; i < xpTotalToReachLevel.length; i++) {
+			xpRequiredForNextLevel[i - 1] = incr;
+			xpTotalToReachLevel[i] = xpTotalToReachLevel[i - 1] + incr;
+			incr += (i % 2 == 0) ? 4 : 3;
+		}
+	}
+
+	/**
+	 * Create a new ExperienceManager for the given player.
+	 * 
+	 * @param player	The player for this ExperienceManager object
+	 */
+	public ExperienceManager(Player player) {
+		this.playerName = player.getName();
+		getPlayer();	// ensure it's a valid player name
+	}
+
+	/**
+	 * Get the Player associated with this ExperienceManager.
+	 * 
+	 * @return	the Player object
+	 * @throws IllegalStateException if the player is no longer online
+	 */
+	public Player getPlayer() {
+		Player p = Bukkit.getPlayer(playerName);
+		if (p == null) {
+			throw new IllegalStateException("Player " + playerName + " is not online");
+		}
+		return p;
+	}
+
+	/**
+	 * Set the players experience
+	 * @param amt		Amount of XP
+	 */
+	public void setExp(int amt) {
+		int xp = amt;
+		if (xp < 0) xp = 0;
+
+		Player player = getPlayer();
+		int curLvl = player.getLevel();
+		int newLvl = getLevelForExp(xp); 
+		if (curLvl != newLvl) {
+			player.setLevel(newLvl);
+		}
+
+		float pct = ((float)(xp - getXpForLevel(newLvl)) / (float)xpRequiredForNextLevel[newLvl]);
+		player.setExp(pct);
+	}
+	
+	/**
+	 * Adjust the player's XP by the given amount in an intelligent fashion.  Works around
+	 * some of the non-intuitive behaviour of the basic Bukkit player.giveExp() method.
+	 * 	
+	 * @param amt		Amount of XP, may be negative
+	 */
+	public void changeExp(int amt) {
+		int xp = getCurrentExp() + amt;
+		if (xp < 0) xp = 0;
+
+		Player player = getPlayer();
+		int curLvl = player.getLevel();
+		int newLvl = getLevelForExp(xp); 
+		if (curLvl != newLvl) {
+			player.setLevel(newLvl);
+		}
+
+		float pct = ((float)(xp - getXpForLevel(newLvl)) / (float)xpRequiredForNextLevel[newLvl]);
+		player.setExp(pct);
+	}
+
+	/**
+	 * Get the player's current XP total.
+	 * 
+	 * @return	the player's total XP
+	 */
+	public int getCurrentExp() {
+		Player player = getPlayer();
+		int lvl = player.getLevel();
+		return getXpForLevel(lvl) + (int) (xpRequiredForNextLevel[lvl] * player.getExp());
+	}
+
+	/**
+	 * Checks if the player has the given amount of XP.
+	 * 
+	 * @param amt	The amount to check for.
+	 * @return		true if the player has enough XP, false otherwise
+	 */
+	public boolean hasExp(int amt) {
+		return getCurrentExp() >= amt;
+	}
+
+	/**
+	 * Get the level that the given amount of XP falls within.
+	 * 
+	 * @param exp	The amount to check for.
+	 * @return		The level that a player with this amount total XP would be.
+	 */
+	public int getLevelForExp(int exp) {
+		if (exp <= 0) return 0;
+		int pos = Arrays.binarySearch(xpTotalToReachLevel, exp);
+		return pos < 0 ? -pos - 2 : pos;
+	}
+
+	/**
+	 * Return the total XP needed to be the given level.
+	 * 
+	 * @param level	The level to check for.
+	 * @return	The amount of XP needed for the level.
+	 */
+	public int getXpForLevel(int level) {
+		if (level >= xpTotalToReachLevel.length) {
+			initLookupTables(level * 2);
+		}
+		return xpTotalToReachLevel[level];
+	}
+}
