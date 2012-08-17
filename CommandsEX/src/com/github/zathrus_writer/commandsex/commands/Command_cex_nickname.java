e480554fee3f997ee6d69604bdf14ea957ee90a2
iKeirNez <keir@nellyer.co.uk>
2012-08-17 19:05:00 +0100

    Added the long awaited nicknames support, YAY

---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_nickname.java
@@ -0,0 +1,76 @@
+package com.github.zathrus_writer.commandsex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.zathrus_writer.commandsex.helpers.Commands;
+import com.github.zathrus_writer.commandsex.helpers.LogHelper;
+import com.github.zathrus_writer.commandsex.helpers.Nicknames;
+import com.github.zathrus_writer.commandsex.helpers.Utils;
+
+public class Command_cex_nickname extends Nicknames {
+
+	/***
+	 * Nickname - Sets a players nickname
+	 * @param sender
+	 * @param alias
+	 * @param args
+	 * @return
+	 */
+	
+	public static Boolean run(CommandSender sender, String alias, String[] args){
+		
+		if (args.length == 0 || args.length > 2 || (args.length == 1 && !(sender instanceof Player))){
+			Commands.showCommandHelpAndUsage(sender, "cex_nickname", alias);
+			return true;
+		}
+		
+		Player target;
+		String nickTo;
+		
+		if (args.length == 1){
+			target = (Player) sender;
+			nickTo = args[0];
+		} else {
+			if (!sender.hasPermission("cex.nickname.others")){
+				LogHelper.showInfo("nickOthersNoPerm", sender, ChatColor.RED);
+				return true;
+			}
+			
+			target = Bukkit.getPlayer(args[0]);
+			if (target == null){
+				LogHelper.showInfo("invalidPlayer", sender, ChatColor.RED);
+				return true;
+			}
+			
+			nickTo = args[1];
+		}
+		
+		if (nickTo.equalsIgnoreCase("reset")){
+			resetNick(target);
+			
+			if (!sender.equals(target)){
+				LogHelper.showInfo("nickResetConfirm#####[" + target.getName(), sender, ChatColor.AQUA);
+				LogHelper.showInfo("[" + sender.getName() + " #####nickResetNotify", target, ChatColor.AQUA);
+			} else {
+				LogHelper.showInfo("nickResetSelf", sender, ChatColor.AQUA);
+			}
+			
+			return true;
+		}
+		
+		setNick(target.getName(), nickTo);
+		
+		if (!sender.equals(target)){
+			LogHelper.showInfo("nickConfirm#####[" + target.getName() + " #####nickSetTo#####[" + Utils.replaceChatColors(nickTo), sender, ChatColor.AQUA);
+			LogHelper.showInfo("[" + sender.getName() + " #####nickNotify#####[" + Utils.replaceChatColors(nickTo), target, ChatColor.AQUA);
+		} else {
+			LogHelper.showInfo("nickSelf#####[" + Utils.replaceChatColors(nickTo), sender, ChatColor.AQUA);
+		}
+		
+		return true;
+	}
+	
+}
---
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/Nicknames.java
@@ -0,0 +1,137 @@
+package com.github.zathrus_writer.commandsex.helpers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+import com.github.zathrus_writer.commandsex.CommandsEX;
+import com.github.zathrus_writer.commandsex.SQLManager;
+
+public class Nicknames implements Listener {
+
+	public Nicknames(){
+		CommandsEX.plugin.getServer().getPluginManager().registerEvents(this, CommandsEX.plugin);
+	}
+	
+	public static void init(CommandsEX plugin){
+		// we can't restore nicknames if the database is disabled
+		if (!CommandsEX.sqlEnabled){
+			return;
+		}
+		
+		// create the tables and columns if they do not exist
+		SQLManager.query((SQLManager.sqlType.equals("mysql") ? "" : "BEGIN; ") + "CREATE TABLE IF NOT EXISTS " + SQLManager.prefix + "nicknames (player_name varchar(32) NOT NULL, nickname varchar(32) NOT NULL)" + (SQLManager.sqlType.equals("mysql") ? " ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores players nicknames'" : "; COMMIT;"));
+		
+		new Nicknames();
+	}
+	
+	/***
+	 * Set a players nickname
+	 * @param pName
+	 * @param nickname
+	 */
+	
+	public static void setNick(String pName, String nickname){
+		// we can't save the nickname if the database is disabled, but we won't disable this feature completely
+		if (CommandsEX.sqlEnabled){
+			// insert or replace nickname into the database
+			ResultSet res = SQLManager.query_res("SELECT player_name, nickname FROM " + SQLManager.prefix + "nicknames WHERE player_name = ?", pName);
+			
+			try {
+				// if a nickname is already in the database, overwrite it otherwise create it
+				if (!res.next()){
+					SQLManager.query("INSERT " + (SQLManager.sqlType.equals("mysql") ? "" : "OR REPLACE ") + "INTO " + SQLManager.prefix + "nicknames (player_name, nickname) SELECT ? AS player_name, ? AS nickname", pName, nickname);
+				} else {
+					SQLManager.query("UPDATE " + SQLManager.prefix + "nicknames SET player_name = ?, nickname = ? WHERE player_name = ?", pName, nickname, pName);
+				}
+			} catch (SQLException e) {
+				if (CommandsEX.getConf().getBoolean("debugMode")){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// if the player is online, set their nickname
+		Player player = Bukkit.getPlayerExact(pName);
+		if (!player.equals(null)){
+			showNick(player, nickname);
+		}
+	}
+	
+	/***
+	 * Shows a players nickname
+	 * @param player
+	 */
+	
+	public static void showNick(Player player){
+		// their default nickname is their player name
+		String nickname = player.getName();
+		
+		if (CommandsEX.sqlEnabled){
+			// get results from db
+			ResultSet res = SQLManager.query_res("SELECT player_name, nickname FROM " + SQLManager.prefix + "nicknames WHERE player_name = ?", player.getName());
+			
+			try {
+				if (res.next()){
+					nickname = res.getString("nickname");
+				}
+				
+				res.close();
+			} catch (SQLException e) {
+				if (CommandsEX.getConf().getBoolean("debugMode")){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		showNick(player, nickname);
+	}
+	
+	/***
+	 * Shows a players nickname
+	 * @param player
+	 * @param nickname
+	 */
+	
+	public static void showNick(Player player, String nickname){
+		// skip if nickname is equal to the players name
+		if (nickname.equals(player.getName())){
+			return;
+		}
+		
+		// allow colours in nicknames
+		nickname = Utils.replaceChatColors(nickname) + ChatColor.RESET;
+		String prefix = CommandsEX.getConf().getString("nicknamePrefix");
+		if (prefix.equals(null)){
+			prefix = "";
+		}
+		
+		// add the prefix
+		nickname = prefix + nickname;
+		
+		// set their display name a TAB list name
+		player.setDisplayName(nickname);
+		player.setPlayerListName(nickname);
+	}
+	
+	/***
+	 * Function to reset a players nickname
+	 * @param player
+	 */
+	
+	public static void resetNick(Player player){
+		String pName = player.getName();
+		
+		// set their display name and TAB list name to their player name
+		player.setDisplayName(pName);
+		player.setPlayerListName(pName);
+		
+		// remove the entry from the database
+		SQLManager.query("DELETE FROM " + SQLManager.prefix + "nicknames WHERE player_name = ?", pName);
+	}
+	
+}
