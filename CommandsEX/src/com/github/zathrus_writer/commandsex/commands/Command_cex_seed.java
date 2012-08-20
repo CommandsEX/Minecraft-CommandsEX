318fb8f411c32bb32336037085183c33c1c7ce33
iKeirNez <keir@nellyer.co.uk>
2012-08-20 17:54:46 +0100

    The seed command now works for /seed overworld, /seed nether, /seed end

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_seed.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_seed.java
@@ -23,15 +23,15 @@ public class Command_cex_seed {
 	 * @param args
 	 * @return
 	 */
-	
+
 	public static Boolean run(CommandSender sender, String alias, String[] args){
 		World world = null;
-		
+
 		if (args.length > 1){
 			Commands.showCommandHelpAndUsage(sender, "cex_seed", alias);
 			return true;
 		}
-		
+
 		if (args.length == 0){
 			if (sender instanceof Player){
 				Player player = (Player) sender;
@@ -41,25 +41,65 @@ public class Command_cex_seed {
 				return true;
 			}
 		} else {
-			List<World> matches = ClosestMatches.world(args[0]);
-			
-			if (matches.size() == 0){
-				List<String> worldNames = new ArrayList<String>();
-				for (World w : Bukkit.getWorlds()){
-					worldNames.add(w.getName());
+			if (sender instanceof Player && (args[0].equalsIgnoreCase("nether") || args[0].equalsIgnoreCase("end")
+					|| args[0].equalsIgnoreCase("theend") || args[0].equalsIgnoreCase("the_end")
+					|| args[0].equalsIgnoreCase("overworld"))){
+				World cWorld = ((Player) sender).getWorld();
+				String worldName = cWorld.getName();
+				if (worldName.contains("_")){
+					worldName = worldName.split("_")[0];
 				}
-				LogHelper.showInfo("seedInvalidWorld", sender, ChatColor.RED);
-				LogHelper.showInfo("seedInvalidWorldList#####[" + ChatColor.GOLD
-						+ Utils.implode(worldNames, ChatColor.AQUA + ", " + ChatColor.GOLD), sender);
-				return true;
+				
+				if (args[0].equalsIgnoreCase("overworld")){
+					World nWorld = Bukkit.getWorld(worldName);
+					if (nWorld != null){
+						world = nWorld;
+					} else {
+						LogHelper.showInfo("seedOverworldNotExist", sender, ChatColor.RED);
+						return true;
+					}
+				}
+				
+				if (args[0].equalsIgnoreCase("nether")){
+					World nWorld = Bukkit.getWorld(worldName + "_nether");
+					if (nWorld != null){
+						world = nWorld;
+					} else {
+						LogHelper.showInfo("seedNetherNotExist", sender, ChatColor.RED);
+						return true;
+					}
+				}
+				
+				if (args[0].contains("end")){
+					World nWorld = Bukkit.getWorld(worldName + "_the_end");
+					if (nWorld != null){
+						world = nWorld;
+					} else {
+						LogHelper.showInfo("seedEndNotExist", sender, ChatColor.RED);
+						return true;
+					}
+				}
+			} else {
+				List<World> matches = ClosestMatches.world(args[0]);
+
+				if (matches.size() == 0){
+					List<String> worldNames = new ArrayList<String>();
+					for (World w : Bukkit.getWorlds()){
+						worldNames.add(w.getName());
+					}
+					LogHelper.showInfo("seedInvalidWorld", sender, ChatColor.RED);
+					LogHelper.showInfo("seedInvalidWorldList#####[" + ChatColor.GOLD
+							+ Utils.implode(worldNames, ChatColor.AQUA + ", " + ChatColor.GOLD), sender);
+					return true;
+				}
+
+				world = ClosestMatches.world(args[0]).get(0);
 			}
-			
-			world = ClosestMatches.world(args[0]).get(0);
 		}
-		
+
 		LogHelper.showInfo("seedShow#####[" + ChatColor.GOLD + world.getName() + ChatColor.AQUA + " #####is#####["
 				+ ChatColor.GOLD + world.getSeed(), sender);
 		return true;
 	}
-	
+
 }
