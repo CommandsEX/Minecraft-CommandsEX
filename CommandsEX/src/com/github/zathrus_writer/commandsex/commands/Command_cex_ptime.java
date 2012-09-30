d3fa2f9264ce1dcf03507f2c508a38d2747b16bb
Keir Nellyer <keir@nellyer.co.uk>
2012-09-30 17:16:48 +0100

    Rewrote /time and /ptime, a lot less bugs and /ptime can now be used on other players.

--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_ptime.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_ptime.java
@@ -1,16 +1,16 @@
 package com.github.zathrus_writer.commandsex.commands;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.github.zathrus_writer.commandsex.CommandsEX;
 import com.github.zathrus_writer.commandsex.helpers.Commands;
 import com.github.zathrus_writer.commandsex.helpers.LogHelper;
-import com.github.zathrus_writer.commandsex.helpers.PlayerHelper;
-import com.github.zathrus_writer.commandsex.helpers.Time;
+import com.github.zathrus_writer.commandsex.helpers.Nicknames;
 import com.github.zathrus_writer.commandsex.helpers.Utils;
 
-public class Command_cex_ptime extends Time {
+public class Command_cex_ptime {
 	/***
 	 * Player Time - changes the players time
 	 * @param sender
@@ -18,24 +18,113 @@ public class Command_cex_ptime extends Time {
 	 * @return
 	 */
 	public static Boolean run(CommandSender sender, String alias, String[] args) {
-		if (PlayerHelper.checkIsPlayer(sender)) {
-			Player player = (Player) sender;
-			// do we have any parameters?
-			
-			if (args.length == 0 || args[0].equalsIgnoreCase("view")){
-				LogHelper.showInfo("playerTimeCurrentTime#####[" + Utils.parseTime(player.getWorld().getTime()) + " #####timeOr#####[" + player.getWorld().getTime() + " #####timeTicks", sender, ChatColor.AQUA);
-			} else if (args.length > 1 || args[0].equalsIgnoreCase("reset")) {
-				if (player.hasPermission("cex.ptime.set")) {
-					setPlayerTime(sender, args);
+		if (sender instanceof Player && Utils.checkCommandSpam((Player) sender, "cex_ptime")){
+			return true;
+		}
+		
+		String function = "";
+		Player target = null;
+		long move = 0L;
+		
+		if (args.length == 0){
+			Commands.showCommandHelpAndUsage(sender, "cex_ptime", alias);
+			return true;
+		}
+
+		if (args.length > 0){
+			function = args[0];
+
+			if (function.equalsIgnoreCase("reset")){
+				if (args.length == 2){
+					target = Bukkit.getPlayer(args[1]);
 				} else {
-					LogHelper.showInfo("playerTimeSetNoPerm", sender, ChatColor.RED);
+					if (!(sender instanceof Player) && args.length != 0){
+						Commands.showCommandHelpAndUsage(sender, "cex_ptime", alias);
+						return true;
+					} else {
+						target = (Player) sender;
+					}
+				}
+				
+				if (target == null){
+					LogHelper.showWarning("invalidPlayer", sender);
+					return true;
+				}
+				
+				if (sender != target && !sender.hasPermission("cex.ptime.others")){
+					LogHelper.showWarning("playerTimeOthersNoPerm", sender);
+					return true;
+				}
+				
+				target.resetPlayerTime();
+				LogHelper.showInfo("playerTimeReset", sender);
+				
+				return true;
+			} 
+			
+			if (args.length == 3){
+				target = Bukkit.getPlayer(args[2]);
+			} else if (args.length == 2){
+				if (sender instanceof Player){
+					target = (Player) sender;
+				} else {
+					Commands.showCommandHelpAndUsage(sender, "cex_ptime", alias);
+					return true;
 				}
 			} else {
-				// show usage
 				Commands.showCommandHelpAndUsage(sender, "cex_ptime", alias);
+				return true;
 			}
 		}
 
+		if (target == null){
+			LogHelper.showWarning("invalidPlayer", sender);
+			return true;
+		}
+		
+		if (target != sender && !sender.hasPermission("cex.ptime.others")){
+			LogHelper.showWarning("playerTimeOthersNoPerm", sender);
+			return true;
+		}
+
+		if (function.equalsIgnoreCase("add")){
+			if (args.length == 2 || args.length == 3){
+				if (!args[1].matches(CommandsEX.intRegex)){
+					LogHelper.showWarning("timeAddOnlyWorksWithNumbers", sender);
+					return true;
+				}
+				
+				move = Long.parseLong(args[1]);
+			}
+			
+			target.setPlayerTime(target.getPlayerTime() + move, false);
+		} else if (function.equalsIgnoreCase("set")){
+			if (args[1].equalsIgnoreCase("dawn") || args[1].equalsIgnoreCase("day")){
+				move = 0L;
+			} else if (args[1].equalsIgnoreCase("morning")){
+				move = 2500L;
+			} else if (args[1].equalsIgnoreCase("noon")){
+				move = 6000L;
+			} else if (args[1].equalsIgnoreCase("evening")){
+				move = 11000L;
+			} else if (args[1].equalsIgnoreCase("night")){
+				move = 15000L;
+			} else if (args[1].matches(CommandsEX.intRegex)){
+				move = Long.parseLong(args[1]);
+			} else {
+				LogHelper.showWarning("timeInvalidStringValue", sender);
+				return true;
+			}
+
+			target.setPlayerTime(move, false);
+		} else {
+			Commands.showCommandHelpAndUsage(sender, "cex_ptime", alias);
+			return true;
+		}
+
+		// tell the player and anyone with permission to see what happened
+		LogHelper.showInfo("playerTimeChanged#####[" + target.getPlayerTime() + " #####timeTicks" + (sender != target ? "#####for#####[ " + Nicknames.getNick(target.getName()) : ""), sender);
+		
         return true;
 	}
 }
--- CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_time.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/commands/Command_cex_time.java
@@ -1,51 +1,128 @@
 package com.github.zathrus_writer.commandsex.commands;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.github.zathrus_writer.commandsex.CommandsEX;
 import com.github.zathrus_writer.commandsex.helpers.ClosestMatches;
 import com.github.zathrus_writer.commandsex.helpers.Commands;
 import com.github.zathrus_writer.commandsex.helpers.LogHelper;
-import com.github.zathrus_writer.commandsex.helpers.Time;
+import com.github.zathrus_writer.commandsex.helpers.Nicknames;
+import com.github.zathrus_writer.commandsex.helpers.Permissions;
 import com.github.zathrus_writer.commandsex.helpers.Utils;
 
-public class Command_cex_time extends Time {
+public class Command_cex_time {
 	/***
-	 * TIME - changes time in the current world
+	 * TIME - changes time in a world
 	 * @param sender
 	 * @param args
 	 * @return
 	 */
 	public static Boolean run(CommandSender sender, String alias, String[] args) {
-		World world;
-
-		// do we have any parameters?
-		if (args.length == 0 || args.length == 1 || args[0].equalsIgnoreCase("view")){
-			if (args.length == 0){
-				if (sender instanceof Player){
-					world = ((Player) sender).getWorld();
+		if (sender instanceof Player && Utils.checkCommandSpam((Player) sender, "cex_time")){
+			return true;
+		}
+		
+		String function = "";
+		World cWorld = (sender instanceof Player ? ((Player) sender).getWorld() : Bukkit.getWorlds().get(0));
+		World w = null;
+		long move = 0L;
+		
+		if (args.length == 0){
+			Commands.showCommandHelpAndUsage(sender, "cex_time", alias);
+			return true;
+		}
+		
+		if (args.length > 0){
+			function = args[0];
+			
+			if (function.equalsIgnoreCase("view")){
+				if (args.length == 2){
+					List<World> matches = ClosestMatches.intellWorld(args[1], cWorld);
+					if (matches.size() == 0){
+						LogHelper.showWarning("invalidWorld", sender);
+						return true;
+					}
+					
+					w = matches.get(0);
+				} else if (args.length == 1){
+					w = cWorld;
 				} else {
-					world = Bukkit.getWorlds().get(0);
+					Commands.showCommandHelpAndUsage(sender, "cex_time", alias);
+					return true;
 				}
 			} else {
-				world = ClosestMatches.intellWorld(args[0], (sender instanceof Player ? ((Player) sender).getWorld() : Bukkit.getWorlds().get(0))).get(0);
+				if (args.length == 3){
+					List<World> matches = ClosestMatches.intellWorld(args[2], cWorld);
+					if (matches.size() == 0){
+						LogHelper.showWarning("invalidWorld", sender);
+						return true;
+					}
+					
+					w = matches.get(0);
+				} else if (args.length == 2){
+					w = cWorld;
+				} else {
+					Commands.showCommandHelpAndUsage(sender, "cex_time", alias);
+					return true;
+				}
+			}
+		}
+		
+		if (w == null){
+			LogHelper.showWarning("invalidWorld", sender);
+			return true;
+		}
+		
+		if (function.equalsIgnoreCase("view")){
+			LogHelper.showInfo("timeCurrentTime1#####[" + Utils.parseTime(w.getTime()) + " #####timeCurrentTime2#####[" + w.getName(), sender);
+			return true;
+		} else if (function.equalsIgnoreCase("add")){
+			if (args.length == 2 || args.length == 3){
+				if (!args[1].matches(CommandsEX.intRegex)){
+					LogHelper.showWarning("timeAddOnlyWorksWithNumbers", sender);
+					return true;
+				}
+				
+				move = Long.parseLong(args[1]);
 			}
 			
-			LogHelper.showInfo("timeCurrentTime1#####[" + Utils.parseTime(world.getTime()) + " #####timeOr#####[" + world.getTime() + " #####timeTicks", sender, ChatColor.AQUA);
-		} else if (args.length > 1) {
-			if (sender.hasPermission("cex.time.set")) {
-				setTime(sender, args);
+			w.setTime(w.getTime() + move);
+		} else if (function.equalsIgnoreCase("set")){
+			if (args[1].equalsIgnoreCase("dawn") || args[1].equalsIgnoreCase("day")){
+				move = 0L;
+			} else if (args[1].equalsIgnoreCase("morning")){
+				move = 2500L;
+			} else if (args[1].equalsIgnoreCase("noon")){
+				move = 6000L;
+			} else if (args[1].equalsIgnoreCase("evening")){
+				move = 11000L;
+			} else if (args[1].equalsIgnoreCase("night")){
+				move = 15000L;
+			} else if (args[1].matches(CommandsEX.intRegex)){
+				move = Long.parseLong(args[1]);
 			} else {
-				LogHelper.showInfo("timeSetNoPerm", sender, ChatColor.RED);
+				LogHelper.showWarning("timeInvalidStringValue", sender);
+				return true;
 			}
+
+			w.setTime(move);
 		} else {
-			// show usage
 			Commands.showCommandHelpAndUsage(sender, "cex_time", alias);
+			return true;
 		}
 
+		// tell the player and anyone with permission to see what happened
+		LogHelper.showInfo("timeChanged#####[" + Utils.parseTime(w.getTime()) + " #####timeOr#####[" + w.getTime() + " #####timeTicks" + (!cWorld.equals(w) ? "#####in#####[ " + w.getName() : ""), sender);
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (!player.equals(sender) && Permissions.checkPermEx(player, "cex.time.notify")) {
+				LogHelper.showInfo("timeChangedBy1#####[" + w.getName() + " #####timeChangedBy2#####[" + Nicknames.getNick(sender.getName()), player);
+			}
+		}
 		return true;
 	}
 }
