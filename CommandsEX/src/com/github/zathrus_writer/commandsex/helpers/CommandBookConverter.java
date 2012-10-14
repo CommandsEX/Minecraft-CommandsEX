a185c5206c9075f617efa9458748fd0f5d93af70
Keir Nellyer <keir@nellyer.co.uk>
2012-10-14 16:52:47 +0100

    Added Warps converting to the CommandBook converter

--- CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/CommandBookConverter.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/helpers/CommandBookConverter.java
@@ -22,15 +22,32 @@ import com.github.zathrus_writer.commandsex.CommandsEX;
 public class CommandBookConverter {
 
 	public static void init(CommandsEX plugin){
-		
+
 		/**
-		 * Begin importing of Homes
+		 * Frequently Used Variables
 		 */
-		
+
+		// create the oldConversionsFolder if it doesn't already exist
+		File oldConversionsFolder = new File(plugin.getDataFolder() + "/old-conversions");
+		if (!oldConversionsFolder.exists()){
+			oldConversionsFolder.mkdir();
+		}
+
+		// generate part of the filename by the year, month, day, hour, minute, second and millisecond
+		// this gives us unique names and makes sure that there are no filename clashes
+		Calendar cal = Calendar.getInstance();
+		String dateString = cal.get(Calendar.YEAR) + "" + cal.get(Calendar.MONTH) + "" + cal.get(Calendar.DAY_OF_MONTH) + "" 
+				+ cal.get(Calendar.HOUR_OF_DAY) + "" + cal.get(Calendar.MINUTE) + "" + cal.get(Calendar.SECOND) + "" + cal.get(Calendar.MILLISECOND);
+
+		/**
+		 * Home Importing
+		 */
+
 		File cmdBkHomes = new File(plugin.getDataFolder() + "/convert/CommandBookHomes.csv");
 		if (cmdBkHomes.exists()){
 			try {
 				LogHelper.logInfo("Started CommandBook Home Conversion");
+				File oldConversion = new File(plugin.getDataFolder() + "/old-conversions/CommandBookHomes" + dateString + ".csv");
 				// begin reading the CSV file
 				BufferedReader CSVFile = new BufferedReader(new FileReader(cmdBkHomes.getAbsolutePath()));
 				// Read the first line
@@ -41,12 +58,17 @@ public class CommandBookConverter {
 					// split the rows values into an array
 					String[] dataArray = dataRow.split(",");
 
-					// get some variables fromt the CSV file
+					// remove the first and last " on all arguments
+					for (int i = 0; i < dataArray.length; i++){
+						dataArray[i] = dataArray[i].substring(1, dataArray[i].length() - 1);
+					}
+					
+					// get some variables from the CSV file
 					String homeName = dataArray[0];
 					String ownerName = dataArray[2];
 					String worldName = dataArray[1];
 					World world = Bukkit.getWorld(worldName);
-					
+
 					// if the world does not exist, don't continue
 					if (world == null){
 						LogHelper.logWarning("Home for " + ownerName + " could not be converted from CommandBook -> CommandsEX");
@@ -57,14 +79,13 @@ public class CommandBookConverter {
 						double z = Double.parseDouble(dataArray[5]);
 						float yaw = Float.parseFloat(dataArray[6]);
 						float pitch = Float.parseFloat(dataArray[7]);
-						
+
 						// construct the location
 						Location l = new Location(world, x, y, z, yaw, pitch);
-						
+
 						// only convert to home if it is the players PRIMARY home as CommandsEX
 						// otherwise we will try to convert it to a private warp
 						if (homeName.equals(ownerName)){
-							LogHelper.logInfo("Started home conversion for " + ownerName);
 							// This sets the players home, deleting all other homes IF allowMultiworldHomes is false
 							Home.setHome(ownerName, l);
 							// alert to console
@@ -80,31 +101,90 @@ public class CommandBookConverter {
 							}
 						}
 					}
-					
-					dataRow = CSVFile.readLine(); // Read next line of data.
+
+					// Read next line of data
+					dataRow = CSVFile.readLine();
 				}
 				// Close the file once all data has been read.
 				CSVFile.close();
 
-				// create the oldConversionsFolder if it doesn't already exist
-				File oldConversionsFolder = new File(plugin.getDataFolder() + "/old-conversions");
-				if (!oldConversionsFolder.exists()){
-					oldConversionsFolder.mkdir();
-				}
-
-				// generate part of the filename by the year, month, day, hour, minute, second and millisecond
-				// this gives us unique names and makes sure that there are no filename clashes
-				Calendar cal = Calendar.getInstance();
-				String dateString = cal.get(Calendar.YEAR) + "" + cal.get(Calendar.MONTH) + "" + cal.get(Calendar.DAY_OF_MONTH) + "" 
-						+ cal.get(Calendar.HOUR_OF_DAY) + "" + cal.get(Calendar.MINUTE) + "" + cal.get(Calendar.SECOND) + "" + cal.get(Calendar.MILLISECOND);
-
-				File oldConversion = new File(plugin.getDataFolder() + "/old-conversions/CommandBookHomes" + dateString + ".csv");
 				// Copy CommandBookHomes.csv to the oldConversion folder, with a unique suffix
 				Utils.copyFile(cmdBkHomes, oldConversion);
 				cmdBkHomes.delete();
 
 				// alert to console
 				LogHelper.logInfo("CommandBook Home Conversion Finished, CommandBookHomes.csv moved to old-conversions");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();// this should never be thrown as we check if the file exists above
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		/**
+		 * Warp Importing
+		 */
+
+		File cmdBkWarps = new File(plugin.getDataFolder() + "/convert/CommandBookWarps.csv");
+
+		if (cmdBkWarps.exists()){
+			try {
+				LogHelper.logInfo("Started CommandBook Warp Conversion");
+				File oldConversion = new File(plugin.getDataFolder() + "/old-conversions/CommandBookWarps" + dateString + ".csv");
+				// begin reading the CSV file
+				BufferedReader CSVFile = new BufferedReader(new FileReader(cmdBkWarps.getAbsolutePath()));
+				// Read the first line
+				String dataRow = CSVFile.readLine();
+
+				// run this, while there are more rows
+				while (dataRow != null){
+					// split the rows values into an array
+					String[] dataArray = dataRow.split(",");
+
+					// remove the first and last " on all arguments
+					for (int i = 0; i < dataArray.length; i++){
+						dataArray[i] = dataArray[i].substring(1, dataArray[i].length() - 1);
+					}
+					
+					// get some variables from the CSV file
+					String warpName = dataArray[0];
+					String ownerName = dataArray[2];
+					String worldName = dataArray[1];
+					World world = Bukkit.getWorld(worldName);
+
+					// if the world does not exist, don't continue
+					if (world == null){
+						LogHelper.logWarning("Warp named " + warpName + " could not be converted from CommandBook -> CommandsEX");
+						LogHelper.logWarning("ERROR: World " + worldName + " does not exist, skipping...");
+					} else {
+						double x = Double.parseDouble(dataArray[3]);
+						double y = Double.parseDouble(dataArray[4]);
+						double z = Double.parseDouble(dataArray[5]);
+						float yaw = Float.parseFloat(dataArray[6]);
+						float pitch = Float.parseFloat(dataArray[7]);
+
+						// construct the location
+						Location l = new Location(world, x, y, z, yaw, pitch);
+
+						// This creates the public warp
+						Warps.createWarp(ownerName, warpName, true, l);
+						// alert to console
+						LogHelper.logInfo("Created warp " + warpName + " successfully!");
+					}
+					
+					// Read next row of data
+					dataRow = CSVFile.readLine();
+				}
+
+				// Close the file once all data has been read.
+				CSVFile.close();
+
+				// Copy CommandBookWarps.csv to the oldConversion folder, with a unique suffix
+				Utils.copyFile(cmdBkWarps, oldConversion);
+				cmdBkWarps.delete();
+
+				// alert to console
+				LogHelper.logInfo("CommandBook Warp Conversion Finished, CommandBookWarps.csv moved to old-conversions");
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();// this should never be thrown as we check if the file exists above
 			} catch (IOException e) {
