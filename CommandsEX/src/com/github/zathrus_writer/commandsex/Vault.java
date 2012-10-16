a59ce853eda561083b62dbdc894f1528a6957271
Keir Nellyer <keir@nellyer.co.uk>
2012-10-16 22:23:51 +0100

    Fixed Vault support breaking when a chat plugin is not detected

--- CommandsEX/src/com/github/zathrus_writer/commandsex/Vault.java
+++ CommandsEX/src/com/github/zathrus_writer/commandsex/Vault.java
@@ -25,27 +25,38 @@ public class Vault {
     }
     
     private Boolean setupEconomy() {
-        if (CommandsEX.plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
+    	if (CommandsEX.plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
+    		System.out.println("Vault Null");
+    		return false;
         }
 
         RegisteredServiceProvider<Economy> rsp = CommandsEX.plugin.getServer().getServicesManager().getRegistration(Economy.class);
+        System.out.println(rsp);
         if (rsp == null) {
             return false;
         }
         
         econ = rsp.getProvider();
+        System.out.println(econ);
         return econ != null;
     }
 
     private Boolean setupChat() {
         RegisteredServiceProvider<Chat> rsp = CommandsEX.plugin.getServer().getServicesManager().getRegistration(Chat.class);
+        if (rsp == null){
+        	return false;
+        }
+        
         chat = rsp.getProvider();
         return chat != null;
     }
 
     private Boolean setupPermissions() {
         RegisteredServiceProvider<Permission> rsp = CommandsEX.plugin.getServer().getServicesManager().getRegistration(Permission.class);
+        if (rsp == null){
+        	return false;
+        }
+        
         perms = rsp.getProvider();
         return perms != null;
     }
